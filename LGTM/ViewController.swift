ac4939e918ef046a94615a7535f8f63882bd13fc
Dongri Jin <dongrify@gmail.com>
2017-11-29 17:41:03 +0900

    Initial Commit

---
+++ LGTM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LGTM
+//
+//  Created by Dongri Jin on 2017/11/29.
+//  Copyright © 2017 Dongri Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
