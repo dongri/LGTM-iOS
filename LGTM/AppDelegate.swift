e4606d214f47ec96f31cb9f5c37fd69e0fa7711f
Dongri Jin <dongrify@gmail.com>
2017-11-30 18:57:25 +0900

    Add: universal links

--- LGTM/AppDelegate.swift
+++ LGTM/AppDelegate.swift
@@ -41,6 +41,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        print("url : \(url.absoluteString)")
+        print("scheme : \(url.scheme!)")
+        print("host : \(url.host!)")
+        //print("port : \(url.port!)")
+        //print("query : \(url.query!)")
+        return true
+    }
 
 }
-
